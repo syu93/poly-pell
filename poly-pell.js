cc36c470178a56dfaf6e5e1ec789a5990a80fa57
Syu93 <herve.tutuaku@gmail.com>
2018-06-23 17:22:36 +0200

    [feature] switch to polymer element

--- poly-pell.js
+++ poly-pell.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from '@polymer/lit-element/lit-element.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import pell from './node_modules/pell/src/pell.js';
 
 /**
@@ -9,21 +9,21 @@ import pell from './node_modules/pell/src/pell.js';
  * @polymer
  * @demo demo/index.html
  */
-export class PolyPell extends LitElement {
+export class PolyPell extends PolymerElement {
   static get is() { return 'poly-pell'; }
-  _render() {
+  static get template() {
     return html`
       <style>
         :host {
           display: block;
           padding: 16px;
         }
-        #pell {min-height: 50vh;border: solid 1px #000000;background-color: #f7f7f7;}
         .pell{border:1px solid hsla(0,0%,4%,.1)}.pell,.pell-content{box-sizing:border-box}.pell-content{height:300px;outline:0;overflow-y:auto;padding:10px}.pell-actionbar{background-color:#fff;border-bottom:1px solid hsla(0,0%,4%,.1)}.pell-button{background-color:transparent;border:none;cursor:pointer;height:30px;outline:0;width:30px;vertical-align:bottom}.pell-button-selected{background-color:#f0f0f0}
-        .pell-actionbar {height: 48px;box-sizing: content-box;}
-        .pell-actionbar button {color: #000000;border: none;font-size: 1.2em;background-color: #fefefe;width: 48px;height: 48px;cursor: pointer;}
-        .pell-actionbar button.pell-button-selected {font-weight: bold;color: var(--app-primary-color, royalblue);border-bottom: 4px solid var(--app-primary-color, royalblue);box-sizing: border-box;}
-        .pell-content {min-height: 100%;height: 50vh;}
+        #pell {min-height: 50vh;border: solid 1px #000000;background-color: #f7f7f7;@apply --pell-container}
+        .pell-actionbar {height: 48px;box-sizing: content-box;@apply --pell-actionbar}
+        .pell-actionbar button {color: #000000;border: none;font-size: 1.2em;background-color: #fefefe;width: 48px;height: 48px;cursor: pointer;@apply --pell-actionbar-button}
+        .pell-actionbar button.pell-button-selected {font-weight: bold;color: var(--app-primary-color, royalblue);border-bottom: 4px solid var(--app-primary-color, royalblue);box-sizing: border-box;@apply --pell-actionbar-button-selected}
+        .pell-content {min-height: 100%;height: 50vh;@apply --pell-content}
         .pell-content p {margin: 0;margin-bottom: 1em;}
         pre {background-color: #eeeeee;padding: 1em;border-radius: 4px;}
       </style>
